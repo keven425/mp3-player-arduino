0c9892d2e8f379c7024a0417ac12440151e15942
Keven Wang <keven425@gmail.com>
2017-04-12 17:17:18 -0700

    adding libraries, starter code

---
+++ Project 1 Libraries/AudioDevice/example/AudioSerialControl/AudioSerialControl.ino
@@ -0,0 +1,66 @@
+#include <audio_device.h>
+
+/*
+ * Audio Device example. 
+ * 
+ * Demonstrates the use of the AudioDevice class for communicating with mp3 players and FM radio. 
+ * 
+ * Open serial console and send character to trigger the different methods:
+ * p: play
+ * s: pause
+ * ]: next track
+ * [: previous track
+ * 
+ * 1-9: select track 1-9 
+ * +: increase volume
+ * -: decrease volume
+ * 
+ * Alex Olwal 2017
+ * 
+ */
+
+const int PIN_MP3_TX = 6;
+const int PIN_MP3_RX = 7;
+
+AudioDevice audio(PIN_MP3_TX, PIN_MP3_RX, mp3a);
+
+int volume = 127;
+
+void setup() {
+  Serial.begin(115200);
+  audio.initHardware();
+  audio.setVolume(volume);
+}
+
+void loop() {
+
+}
+
+void serialEvent() {
+
+  char c = (char)Serial.read();
+
+  switch (c)
+  {
+    case 'p': audio.play(); break;
+    case 's': audio.pause(); break;
+    case ']': audio.next(); break;
+    case '[': audio.previous(); break;
+
+    case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
+      audio.setTrack(c - '0'); break;
+
+    case '+':
+      volume = min(volume + 10, 255); 
+      audio.setVolume(volume);
+      break;
+
+    case '-':
+      volume = max(volume - 10, 0); 
+      audio.setVolume(volume);
+      break;
+
+
+  }
+}
---
+++ bpm_mp3/bpm_mp3.ino
@@ -0,0 +1,230 @@
+/*
+ * State Machine example using structs + EventManager library.
+ * 
+ * Alex Olwal 2017
+ * 
+ */
+
+#include <Wire.h>
+#include "EventManager.h"
+
+//The system has three events
+const int EVENT_0 = 0;
+const int EVENT_1 = 1;
+const int EVENT_2 = 2;
+
+EventManager eventManager;
+
+//Defines a custom structure StateMachineDuo (data type), with two states
+struct StateMachineDuo
+{
+  enum State { UNDEFINED, STATE_ONE, STATE_TWO }; //Two states + an (optional) undefined state
+  int currentState = UNDEFINED; //The default is undefined
+  void (* listener)(int event, int parameter); //Function pointer that can point to our callback
+};
+//Create an "alias" [StateMachineDuo] to [struct StateMachineDuo] (better syntax)
+typedef struct StateMachineDuo StateMachineDuo; 
+
+//Defines a custom structure StateMachineDuo (data type), with three states
+struct StateMachineTrio
+{
+  enum State { UNDEFINED, STATE_ONE, STATE_TWO, STATE_THREE };
+  int currentState = UNDEFINED;
+  void (* listener)(int event, int parameter);
+};
+//Create an "alias" [StateMachineTrio] to [struct StateMachineTrio] (better syntax)
+typedef struct StateMachineTrio StateMachineTrio;
+
+//Create three variables representing state machines, using our custom data types.
+//Note that we are using the StateMachineTrio data type twice. (this is the benefit of the struct)
+StateMachineDuo player;
+StateMachineDuo bpm_controller;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.readString(); //Read "away" any potentially buffered data
+
+  //Point the listener function pointer to the respective callback
+  player.listener = on_play_input;
+  bpm_controller.listener = on_bpm_input;
+
+  //Map the different events to the listeners
+  //Note that the toggle state machine is only listening to two events
+  eventManager.addListener(EVENT_BTN_PLAY, player.listener); // start
+  eventManager.addListener(EVENT_BTN_STOP, player.listener); // pause
+  eventManager.addListener(EVENT_BTN_NEXT, player.listener); // next
+  eventManager.addListener(EVENT_BTN_PREV, player.listener); // prev
+  eventManager.addListener(EVENT_DIAL_UP, player.listener); // volume up
+  eventManager.addListener(EVENT_DIAL_DOWN, player.listener); // volume down
+  eventManager.addListener(EVENT_BTN_FAV, player.listener); // volume down
+
+  eventManager.addListener(EVENT_SLIDER_MOVE, bpm_controller.listener); // bpm
+  
+  Serial.println();
+  Serial.println("--------------------------------");
+
+  //Initialize all state machines (Each listener will handle the UNDEFINED state in this example).
+  player.listener(0, 0);
+  bpm_controller.listener(0, 0);
+}
+
+void loop() {
+
+  //Keep track of which of the three events we should be posting
+  static int index = 0;
+
+  //Handle any events that are in the queue
+  eventManager.processEvent();
+
+  //Post events
+
+  Serial.print("------------ [");
+  Serial.print(index);
+  Serial.println("] ------------");
+
+  switch (index)
+  {
+    case 0:
+      eventManager.queueEvent(EVENT_0, 0); 
+      break;
+
+    case 1:
+      eventManager.queueEvent(EVENT_1, 0); 
+      break;
+
+    case 2:
+      eventManager.queueEvent(EVENT_2, 0); 
+      break;
+  }
+
+  //Cycle through { 0, 1, 2 }
+  index = (index + 1) % 3;
+
+  //Wait two seconds 
+  delay(2000);
+
+}
+
+void onPlay( int event, int param )
+{
+  Serial.print("onToggle(   ");
+  Serial.print(event);
+  Serial.print(", ");  
+  Serial.print(param);
+  Serial.print(") current state[");
+  Serial.print(toggle.currentState);
+  Serial.print("]");
+
+  //choose the code block based on the current state
+  // UNDEFINED -> STATE_ONE -> STATE_TWO -> STATE_ONE ...
+  switch (toggle.currentState) {
+    
+    case toggle.UNDEFINED:
+      // Do Intialization things
+      toggle.currentState = toggle.STATE_ONE; //move to the next state
+      break;
+
+    case toggle.STATE_ONE:
+      if (event == EVENT_0) {
+        toggle.currentState = toggle.STATE_TWO;
+      }
+      break;
+
+    case toggle.STATE_TWO:
+      if (event == EVENT_1) {
+        toggle.currentState = toggle.STATE_ONE;
+      }
+      break;
+  }
+
+  Serial.print("->[");
+  Serial.print(toggle.currentState);
+  Serial.println("]");
+}
+
+void onForward( int event, int param )
+{
+  Serial.print("onForward(  ");
+  Serial.print(event);
+  Serial.print(", ");  
+  Serial.print(param);
+  Serial.print(") current state[");
+  Serial.print(forward.currentState);
+  Serial.print("]");
+
+  //choose the code block based on the current state
+  // UNDEFINED -> STATE_ONE -> STATE_TWO -> STATE_THREE -> STATE_ONE ...
+  switch (forward.currentState) {
+
+    case forward.UNDEFINED:
+      // Do Intialization things
+      forward.currentState = forward.STATE_ONE;
+      break;
+
+    case forward.STATE_ONE:
+      if (event == EVENT_0) {
+        forward.currentState = forward.STATE_TWO;
+      }
+      break;
+
+    case forward.STATE_TWO:
+      if (event == EVENT_1) {
+        forward.currentState = forward.STATE_THREE;
+      }
+      break;
+
+    case forward.STATE_THREE:
+      if (event == EVENT_2) {
+        forward.currentState = forward.STATE_ONE;
+      }
+      break;
+  }
+
+  Serial.print("->[");
+  Serial.print(forward.currentState);
+  Serial.println("]");
+}
+
+void onBackwards( int event, int param )
+{
+  Serial.print("onBackwards(");
+  Serial.print(event);
+  Serial.print(", ");  
+  Serial.print(param);
+  Serial.print(") current state[");
+  Serial.print(backwards.currentState);
+  Serial.print("]");
+
+  //choose the code block based on the current state
+  // UNDEFINED -> STATE_THREE -> STATE_TWO -> STATE_ONE -> STATE_THREE ...
+  switch (backwards.currentState) {
+
+    case backwards.UNDEFINED:
+      // Do Intialization things
+      backwards.currentState = backwards.STATE_THREE;
+      break;
+
+    case backwards.STATE_ONE:
+      if (event == EVENT_2) {
+        backwards.currentState = backwards.STATE_THREE;
+      }
+      break;
+
+    case backwards.STATE_TWO:
+      if (event == EVENT_1) {
+        backwards.currentState = backwards.STATE_ONE;
+      }
+      break;
+
+    case backwards.STATE_THREE:
+      if (event == EVENT_0) {
+        backwards.currentState = backwards.STATE_TWO;
+      }
+      break;
+  }
+
+  Serial.print("->[");
+  Serial.print(backwards.currentState);
+  Serial.println("]");
+}
+
